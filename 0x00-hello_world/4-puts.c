5997e36cf886958d3a76e9766a4644fdeb4c4853
Paul <paulodhis11@gmail.com>
2023-06-15 14:47:36 -0700

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - C program that prints exactly Programming
+ * Return: 0 success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
