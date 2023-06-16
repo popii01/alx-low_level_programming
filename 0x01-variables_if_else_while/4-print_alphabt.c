429be94d331ed564fc420e7af9edd454f03a558d
Paul <paulodhis11@gmail.com>
2023-06-16 15:01:45 -0700

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - for entry points
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'a') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
