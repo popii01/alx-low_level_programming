2742ae7e5c6dc160ffc9f1e26ef3ff92d55966d0
Paul <paulodhis11@gmail.com>
2023-06-20 15:24:16 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,6 +13,8 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
+			if ((a <= 1 && b<== 9) || (a <= 2 && b <= 3))
+		{
 			for (c = 0; c <= 5; c++)
 			{
 				for (d = 0; d <= 9; d++)
@@ -27,4 +29,5 @@ void jack_bauer(void)
 			}
 		}
 	}
+}
 }
